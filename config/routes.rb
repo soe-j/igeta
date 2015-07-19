7bb1f9b90884d4c747774dc929fcdc0ea7c22bb3
soe-j <soeda@val.co.jp>
2015-07-19 23:05:36 +0900

    create sessions controller and create view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'general#index'
+
+  get '/auth/twitter/callback', to: 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

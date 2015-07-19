06d225a8a3dc075d696c8bbe415bb370737085b6
soe-j <soeda@val.co.jp>
2015-07-19 23:37:58 +0900

    user info are saved to cookies

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    @auth = request.env['omniauth.auth']
+    auth = request.env['omniauth.auth']
+    cookies[:user_name] = auth.info.nickname
+    cookies[:user_image] = auth.info.image
+    cookies[:user_url] = auth.info.urls.Twitter
+    cookies[:twitter_token] = auth.credentials.token
+    cookies[:twitter_secret] = auth.credentials.secret
+
+    redirect_to root_path
   end
 end
